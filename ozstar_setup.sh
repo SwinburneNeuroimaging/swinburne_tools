2d0ccb807d7fbf3b13fd82dfd3b37c49e940a08a
willw1 <will.woods2@gmail.com>
2025-02-12 13:11:53 +1100

    Update ozstar_setup.sh

--- ozstar_setup.sh
+++ ozstar_setup.sh
@@ -14,10 +14,30 @@ sudo chmod a+w /fred
 sudo chmod -R a+w /home/$OZSTAR_USER
 sudo chmod a+w /dagg
 SSHFS_OPTS=allow_root,ServerAliveInterval=5,ServerAliveCountMax=3
-echo connect /fred
-sshfs  -o IdentityFile=$HOME/.ssh/id_rsa -o $SSHFS_OPTS $OZSTAR_USER@ozstar.swin.edu.au:/fred /fred
-echo connect /dagg
-sshfs  -o IdentityFile=$HOME/.ssh/id_rsa -o $SSHFS_OPTS $OZSTAR_USER@ozstar.swin.edu.au:/dagg /dagg
-echo connect /home/$OZSTAR_USER
-mkdir -p /home/$OZSTAR_USER
-sshfs  -o IdentityFile=$HOME/.ssh/id_rsa -o $SSHFS_OPTS $OZSTAR_USER@ozstar.swin.edu.au:/home/$OZSTAR_USER /home/$OZSTAR_USER
+
+mkdir -p /home/vdiuser/.local/bin
+echo "#!/bin/bash
+
+if ! mountpoint -q /fred; then
+    echo connecting /fred
+    sshfs  -o IdentityFile=$HOME/.ssh/id_rsa -o $SSHFS_OPTS $OZSTAR_USER@ozstar.swin.edu.au:/fred /fred
+else
+    echo /fred already connected
+fi
+if ! mountpoint -q /dagg; then
+    echo connecting /dagg
+    sshfs  -o IdentityFile=$HOME/.ssh/id_rsa -o $SSHFS_OPTS $OZSTAR_USER@ozstar.swin.edu.au:/dagg /dagg
+else
+    echo /dagg already connected
+fi
+if ! mountpoint -q /home/$OZSTAR_USER; then
+    echo connecting /home/$OZSTAR_USER
+    sshfs  -o IdentityFile=$HOME/.ssh/id_rsa -o $SSHFS_OPTS $OZSTAR_USER@ozstar.swin.edu.au:/home/$OZSTAR_USER /home/$OZSTAR_USER
+else
+    echo /home/$OZSTAR_USER already connected
+fi
+" > /home/vdiuser/.local/bin/reconnect_ozstar
+
+chmod u+x /home/vdiuser/.local/bin/reconnect_ozstar
+
+/home/vdiuser/.local/bin/reconnect_ozstar
